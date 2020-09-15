50306b87679c03c1b222a15cccd1e452202c630f
Taejoon Park <taejoon.park@gatech.edu>
2020-09-15 01:57:21 -0400

    hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,23 @@
-Not Implemented Yet!
+// We are writing a Pass
+// We are operating on Function
+// We will be doing some printing
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+// Functions from the include files live in the llvm namespace
+using namespace llvm;
+
+// Anonymous namespaces: this will only show to the current file
+namespace {
+	struct Hello : public FunctionPass { // Declares a "Hello" class that is the sublcass of FunctionPass
+		static char ID;
+		Hello() : FunctionPass(&ID) {} //Declares pass identifier used by LLVM to identify pass. This allwos LLVM to avoid using C++ runtime
+		virtual bool runOnFunction(Function &F) { //we declare "runOnFunction": overloads an abstract virtual method inherited from FunctionPass
+			err() << "Hello: " << F.getName() << "\n";
+			return false;
+		}
+	}
+	char Hello::ID = 0; // LLVM uses ID's address to identify pass so initialization value is not important
+	INITIALIZE_PASS(Hello, "hello", "Hello World Pass", false /*Oly looks at CFG */, false /* Analysis Pass*/);
+}
